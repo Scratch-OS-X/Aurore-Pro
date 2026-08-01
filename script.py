83c073b6e2d68e0c7014b6a8614c89eec861b214
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-08-01 17:16:22 +0200

    Initial commit

---
+++ script.py
@@ -0,0 +1,56 @@
+import math
+import datetime as dt
+
+print("Aurore Pro Py [BÊTA]")
+print("Tape 'help' pour une liste des commandes.")
+
+while True:
+    aurore = input("\n>>> ")
+
+    if aurore == "help":
+        print("Tape 'quit' pour quitter")
+        print("Tape 'time' pour obtenir la date et l'heure actuelle")
+    elif aurore == "quit":
+        break
+
+    elif aurore == "time":
+        now = dt.datetime.now()
+        print(f"Nous sommes le {now.strftime('%d/%m/%Y')} et il est {now.strftime('%H:%M:%S')}")
+
+    elif aurore == "+" :
+      nb1 = input("Nombre 1 >>> ")
+      nb2 = input("Nombre 2 >>> ")
+      result = float(nb1) + float(nb2)
+      if str(result).endswith(".0"):
+        result = int(result)
+      print(f"""{nb1} + {nb2} = {result}""")
+
+    elif aurore == "-" :
+      nb1 = input("Nombre 1 >>> ")
+      nb2 = input("Nombre 2 >>> ")
+      result = float(nb1) - float(nb2)
+      if str(result).endswith(".0"):
+        result = int(result)
+      print(f"""{nb1} - {nb2} = {result}""")
+
+    elif aurore == "*" or aurore == "x" :
+      nb1 = input("Nombre 1 >>> ")
+      nb2 = input("Nombre 2 >>> ")
+      result = float(nb1) * float(nb2)
+      if str(result).endswith(".0"):
+        result = int(result)
+      print(f"""{nb1} * {nb2} = {result}""")
+
+    elif aurore == "/" or aurore == ":" or aurore == "÷" :
+      nb1 = input("Nombre 1 >>> ")
+      nb2 = input("Nombre 2 >>> ")
+      if float(nb2) != 0:
+        result = float(nb1) / float(nb2)
+        if str(result).endswith(".0"):
+          result = int(result)
+        print(f"""{nb1} ÷ {nb2} = {result}""")
+      else:
+        print("\033[91m Erreur: Division par zéro impossible \033[0m")
+ 
+    else:
+      print("\033[91m Indisponible \033[0m")
